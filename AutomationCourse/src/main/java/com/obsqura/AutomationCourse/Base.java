ba0d5de732041cd496e09f46ea1676ab6d8114f8
Poojajishnu <mailtopoojasr@gmail.com>
2023-05-16 21:17:10 +0530

    second commit

--- AutomationCourse/src/main/java/com/obsqura/AutomationCourse/Base.java
+++ AutomationCourse/src/main/java/com/obsqura/AutomationCourse/Base.java
@@ -12,11 +12,50 @@ public class Base {
 		 driver=new ChromeDriver();
 		 driver.get("https://www.amazon.in/");
 	}
+	public void driverClose()
+	{
+		driver.close();
+	}
+	public void driverQuit()
+	{
+		driver.quit();
+	}
+	public void gettitleAndPrint()
+	{
+		String expectedTitle="Online Shopping site in India: Shop Online for Mobiles, Books, Watches, Shoes and More - Amazon.in";
+		String actualTitle=driver.getTitle();  
+		System.out.println("expected title is:"+expectedTitle);
+		if(actualTitle.equals(expectedTitle))
+		{
+			System.out.println("test passed");
+		}
+		else
+		{
+			System.out.println("test failed");
+		}
+		
+	}
+	
+	public void getPageSourceAdd()
+	{
+		String pageSource=driver.getPageSource();
+	}
+	public void getUrl()
+	{
+	driver.getCurrentUrl();
+	}
+	
 	
 	public static void main(String args[])
 	{
 	Base base=new Base();
 	base.initializeBrowser();
+	base.gettitleAndPrint();
+	base.getPageSourceAdd();
+	base.getUrl();
+	base.driverQuit();
+	
+	
 	}
 
 }
