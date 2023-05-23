1537983fb9ed2defec6a7417777de63c2a87c5af
Poojajishnu <mailtopoojasr@gmail.com>
2023-05-23 13:18:12 +0530

    next

--- AutomationCourse/src/main/java/com/obsqura/AutomationCourse/FireFoxBrowser.java
+++ AutomationCourse/src/main/java/com/obsqura/AutomationCourse/FireFoxBrowser.java
@@ -10,12 +10,56 @@ public class FireFoxBrowser {
 		System.setProperty("webdriver.firefox.driver","D:\\Eclipse Sample\\AutomationCourse\\src\\main\\java\\resources\\geckodriver.exe");
 		driver=new FirefoxDriver();
 		driver.get("https://www.amazon.in/");
+		driver.manage().window().maximize();
+	}
+	public void driverClose()
+	{
+		driver.close();
+	}
+	public void titleGet()
+	{
+	String expectedTitle="Online Shopping Site for Mobiles, Electronics, Furniture, Grocery, Lifestyle, Books & More. Best Offers!";
+	String actualTitle=driver.getTitle();
+	System.out.println("expected result is:"+expectedTitle);
+	System.out.println("actual result is:"+actualTitle);
+	if(expectedTitle.equals(actualTitle))
+	{
+		System.out.println("test passed");
+	}
+	else
+	{
+		System.out.println("test failed");
+	}
+	}
+	public void firefoxdriver()
+	{
+		String pagesource=driver.getPageSource();
+	}
+	public void UrlGet()
+	{
+		driver.getCurrentUrl();
+	}
+	public void naviagationCommands()
+	{
+		driver.navigate().to("https://www.amazon.in/");
+		
+		driver.navigate().back();
+		driver.navigate().forward();
+		driver.navigate().refresh();
+	}
+	public void driverQuit()
+	{
+		driver.quit();
 	}
 	public static void main(String args[])
 	{
 		FireFoxBrowser firefoxdriver=new FireFoxBrowser();
 		firefoxdriver.initializeBrowser();
-		
+		//firefoxdriver.driverClose();
+		firefoxdriver.titleGet();
+		firefoxdriver.firefoxdriver();
+		firefoxdriver.UrlGet();
+		firefoxdriver.naviagationCommands();
+		firefoxdriver.driverQuit();		
 	}
-
 }
